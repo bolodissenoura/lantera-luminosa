1d1691faab1c14f141f593172c32cbd770e452ab
Pansutti <pansuttitorres@hotmail.com>
2024-01-12 21:12:14 -0300

    feat: arrumando

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
