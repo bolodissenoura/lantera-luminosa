db39c85afa69561385694eb9a6c98281c50ca623
daniellima <Daniel.Silva.ext@cyrela.com.br>
2024-01-12 21:19:56 -0300

    fix: theme provider error

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,10 @@
-import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
-import { TranslationProvider } from '../components/TranslationContext';
+import { TranslationProvider } from "../components/TranslationContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <TranslationProvider>
-    <ThemeProvider attribute="class">
       <Component {...pageProps} />
-    </ThemeProvider>
     </TranslationProvider>
   );
 }
