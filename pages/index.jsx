642365a8f1fbef33a36ca4919cc6edc5e5a660a8
JuanLucca846 <lucca470@gmail.com>
2024-03-27 23:25:19 -0300

    fix: Section apoiadores

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,6 @@ import Image from "next/image";
 import Contribuidores from "../components/contribuidores";
 import Vaquinha from "../components/Vaquinha";
 
-
 const Home = () => {
   return (
     <>
@@ -23,12 +22,12 @@ const Home = () => {
       <Navbar />
       <Hero />
       <Image
-      src="/img/wave.svg"
-      width={1440}
-      height={389}
-      className="absolute w-full"
-      alt="WaveImg"
-    />
+        src="/img/wave.svg"
+        width={1440}
+        height={389}
+        className="absolute w-full"
+        alt="WaveImg"
+      />
       <SectionTitle
         titleClassName="pt-[110px] md:pt-[300px]"
         title="Sobre nós"
@@ -54,28 +53,26 @@ const Home = () => {
           públicas, mistas e privadas, secretarias governamentais, poder
           legislativo, judiciário e outras organizações do terceiro setor."
       ></SectionTitle>
-      <Contribuidores />
+
       <div className="bg-[#EDEDED]">
-      <Benefits data={benefitOne} />
-      <Benefits imgPos="right" data={benefitTwo} />
-      <Vaquinha />
+        <Benefits data={benefitOne} />
+        <Benefits imgPos="right" data={benefitTwo} />
+        <Vaquinha />
       </div>
-      <SectionTitle
-        pretitle="Watch a video"
-        title="Nossos vídeos"
-      >
+      <SectionTitle pretitle="Watch a video" title="Nossos vídeos">
         This section is to highlight a promo or demo video of your product.
         Analysts says a landing page with video has 3% more conversion rate. So,
         don&apos;t forget to add one. Just like this.
       </SectionTitle>
-      <Video src="https://www.youtube-nocookie.com/embed/VQ-6WzP9vS4?controls=0&autoplay=0"
-              src2="https://www.youtube.com/embed/D0UnqGm_miA"/>
-      <Video src="https://www.youtube-nocookie.com/embed/VQ-6WzP9vS4?controls=0&autoplay=0"
-       src2="https://www.youtube.com/embed/D0UnqGm_miA" />
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Depoimentos"
-      >
+      <Video
+        src="https://www.youtube-nocookie.com/embed/VQ-6WzP9vS4?controls=0&autoplay=0"
+        src2="https://www.youtube.com/embed/D0UnqGm_miA"
+      />
+      <Video
+        src="https://www.youtube-nocookie.com/embed/VQ-6WzP9vS4?controls=0&autoplay=0"
+        src2="https://www.youtube.com/embed/D0UnqGm_miA"
+      />
+      <SectionTitle pretitle="Testimonials" title="Depoimentos">
         Testimonails is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
       </SectionTitle>
@@ -85,7 +82,8 @@ const Home = () => {
         conversion rate as well as support or chat requests.
       </SectionTitle>
       <Faq />
-      <Cta/>
+      <Cta />
+      <Contribuidores />
       <Footer />
       <PopupWidget />
     </>
