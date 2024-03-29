9e8293dc88b6549b28079dd22694207ac9494464
claudinxch <claudiohenrique0909@gmail.com>
2024-03-29 12:10:38 -0300

    Fix #29 Seção de vídeos duplicada e Feat #38 adicionar CTA abaixo dos vídeos

--- pages/index.jsx
+++ pages/index.jsx
@@ -67,10 +67,6 @@ const Home = () => {
         src="https://www.youtube-nocookie.com/embed/VQ-6WzP9vS4?controls=0&autoplay=0"
         src2="https://www.youtube.com/embed/D0UnqGm_miA"
       />
-      <Video
-        src="https://www.youtube-nocookie.com/embed/VQ-6WzP9vS4?controls=0&autoplay=0"
-        src2="https://www.youtube.com/embed/D0UnqGm_miA"
-      />
       <SectionTitle pretitle="Testimonials" title="Depoimentos">
         Testimonails is a great way to increase the brand trust and awareness.
         Use this section to highlight your popular customers.
