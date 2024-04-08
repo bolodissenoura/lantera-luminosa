b1af1d43bbf04c9d87050359d59ea4ad892b88b0
Júlio César Wanderosfky Pedro <juliowanke@gmail.com>
2024-04-08 10:24:39 -0300

    remove repeated link

--- components/cta.jsx
+++ components/cta.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from './container';
 import usePopupStore from './popupStore';
 import Link from 'next/link';
-import Link from 'next/link';
 
 const Cta = () => {
   const { togglePopup } = usePopupStore();
