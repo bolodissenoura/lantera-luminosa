b05a43ca373445fc486471621f7d4ffeac42e144
Maycon Padilha <maycon.jordan5000@gmail.com>
2024-04-23 11:21:07 -0300

    item 10: adição do item contato no navbar e link para os contatos

--- components/cta.jsx
+++ components/cta.jsx
@@ -8,7 +8,7 @@ const Cta = () => {
 
   return (
     <Container>
-      <div className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-green px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
+      <div id='contato' className="flex flex-wrap items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-green px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap rounded-xl">
         <div className="flex-grow text-center lg:text-left">
           <h2 className="text-2xl font-medium lg:text-3xl">
             Ainda restam dúvidas? Chame a gente!
